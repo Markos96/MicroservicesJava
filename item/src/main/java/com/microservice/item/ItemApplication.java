9bc436480a2ec1588f84e847d783891286400ce0
markos96 <markossolari@gmail.com>
2024-03-30 22:37:42 -0300

    Create microservices and eureka

---
+++ item/src/main/java/com/microservice/item/ItemApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemApplication.class, args);
+	}
+
+}
---
+++ item/src/test/java/com/microservice/item/ItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
